f8d406eaaf167162ef8a76db784306120fae92a1
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-14 21:03:11 -0500

    Started coding
    Mortgage Calculator is started but a long way to go. Also no comments
    have been added.

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Jarrod Smith
+ * 10/8/15
+ * SDI 1510 Section 1
+ * Conditional Assignment
+ */
+
+
+//Variable
+
+var creditScore;
+var principle = 30000;
+var intrest = 4.5;
+var numPayments = 360;
+var monthlyPayment;
+
+//Inputs
+
+creditScore = prompt("Enter your credit score.");
+principle = prompt("Enter the amount for mortgage loan for your home.");
+
+
+//Equations
+
+
+
+//Output
+
+
+
+//Test
