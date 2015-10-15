6dd57256510ce9a474de47c4c2ed64c0db8cc725
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-15 12:34:15 -0500

    continuing to edit
    Continuing to edit code and add more comments.

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ var typePayments = true;        //If this is true you will input years of the lo
 var yearsOfLoan;                //How many year term the loan is.
 var payment;                    //The amount of the monthly payment
 var intrestCompounded;          //Part of the compounding of intrest
-var downPayment = 0;                //The amount of the down payment
+var downPayment = 0;            //The amount of the down payment
 //Inputs
 
 creditScore = Number(prompt("Enter your credit score."));       //Enter the credit score
@@ -26,25 +26,26 @@ if (creditScore === 0){
     creditScore = prompt("Please enter your credit score.")
 }
 
-if (creditScore < 600){
-    intrest = .01;                                    //Intrest rate of %12
+if (creditScore < 600){                                 //If your score is less then 600
+    intrest = .01;                                      //Intrest rate of %12
     console.log("Your intrest rate will be %12.");      //Print interest rate to console
 
-}else if (creditScore < 700){
-    intrest = .006666667;                             //Intrest rate of %8
+}else if (creditScore < 700){                           //if your score is less then 700
+    intrest = .006666667;                               //Intrest rate of %8
     console.log("Your intrest rate will be %8.");       //Print interest rate to console
 
-}else{
-    intrest = .00375;                                 //Intrest rate of %4.5
+}else{                                                  //If your score is anything over 700
+    intrest = .00375;                                   //Intrest rate of %4.5
     console.log("Your intrest rate will be %4.5.");     //Print interest rate to console
 }
 
 //Enter the down payment
 downPayment = Number(prompt("Enter the amount of the down payment."));
-console.log("You entered " + downPayment + " for your down payment.")
+console.log("You entered " + downPayment + " for your down payment.");
 
 //Enter the amount of the loan
 principle = Number(prompt("Enter the amount for mortgage loan for your home."));
+
 //Check to ensure the user entered amount
 if (principle === 0){
     principle = Number(prompt("Please enter the amount for the mortgage loan for your home."))
@@ -54,15 +55,15 @@ if (principle === 0){
 typePayments = confirm("Click \"OK\" for years or click \"Cancel\" to enter months.");           //Confirm the type of payment
 
 if (typePayments === true){
-    yearsOfLoan = Number(prompt("Enter the amount of years of the loan."));     //Number of years for term of loan
+    yearsOfLoan = Number(prompt("Enter the amount of years of the loan."));                     //Number of years for term of loan
         //Check to ensure the user entered amount
         if (yearsOfLoan === 0){
             yearsOfLoan = Number(prompt("Please enter the total number of years of the loan."))
         }
-    monthlyPayment = yearsOfLoan * 12;
+    monthlyPayment = yearsOfLoan * 12;                                                          //Converts years to months
     console.log("You will make a total of " + monthlyPayment + " monthly payments.");
 }else{
-    monthlyPayment = prompt("Enter the number of monthly payments you will be making.");
+    monthlyPayment = prompt("Enter the number of monthly payments you will be making.");        //Enter number of monthly payments
         //Check to ensure the user entered amount
         if (monthlyPayment === ""){
             monthlyPayment = prompt("Please enter the number of monthly payments you will be making.")
